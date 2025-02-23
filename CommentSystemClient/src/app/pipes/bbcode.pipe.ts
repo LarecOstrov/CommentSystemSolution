9702be6e5b24f74d5229d08784b9076e5d261b73
GromusInc <gromusapp@gmail.com>
2025-02-23 23:12:34 +0200

    Update favicon, styles, BBCode, and title
    - Updated `angular.json` to include `src/favicon.ico` in assets for both app and test configurations.
    - Deleted the `favicon.ico` file.
    - Added new styles in `comment-list.component.scss` for `.comment-text code` to handle white-space, overflow, display, and max-width properties.
    - Modified `[code]...[/code]` BBCode transformation in `bbcode.pipe.ts` to use `<code>` instead of `<pre><code>`.
    - Changed the title in `index.html` from "CommentSystemClient" to "Speaking Room".

--- CommentSystemClient/src/app/pipes/bbcode.pipe.ts
+++ CommentSystemClient/src/app/pipes/bbcode.pipe.ts
@@ -12,7 +12,7 @@ export class BbcodePipe implements PipeTransform {
       .replace(/\[strong\](.*?)\[\/strong\]/g, '<strong>$1</strong>')
       .replace(/\[i\](.*?)\[\/i\]/g, '<em>$1</em>')
       .replace(/\[u\](.*?)\[\/u\]/g, '<u>$1</u>')
-      .replace(/\[code\](.*?)\[\/code\]/gs, '<pre><code>$1</code></pre>') 
+      .replace(/\[code\](.*?)\[\/code\]/gs, '<code>$1</code>') 
       .replace(/\[a=(https?:\/\/[^\]]+)\](.*?)\[\/a\]/g, '<a href="$1" target="_blank" rel="noopener noreferrer">$2</a>') 
       .replace(/\[a\](.*?)\[\/a\]/g, '<a href="#">$1</a>') 
       //.replace(/\n/g, '<br>') // Переноси рядків
